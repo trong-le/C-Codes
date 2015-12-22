d311095d4315790ac263361100e42270733bed84
Trong Le <trongle@Trongs-Mac.local>
2015-12-21 16:36:59 -0800

    Unity Sphere Motion CSharp file

---
+++ sphere_motion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphere_motion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("a"))
+			transform.Translate(new Vector3(0.1f, 0, 0));
+		if(Input.GetKey("d"))
+			transform.Translate(new Vector3(-0.1f, 0, 0));
+		if(Input.GetKey("s"))
+			transform.Translate(new Vector3(0, 0, 0.1f));
+		if(Input.GetKey("w"))
+			transform.Translate(new Vector3(0, 0, -0.1f));
+	}
+}
